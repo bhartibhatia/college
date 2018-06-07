74e41290eb06b09037acf438d55d52cc26d763b8
bhartibhatia <38082654+bhartibhatia@users.noreply.github.com>
2018-06-07 16:43:00 +0530

    Add files via upload

---
+++ college/src/College.java
@@ -0,0 +1,14 @@
+
+public class College
+{
+
+    
+    public static void main(String[] args)
+           {
+       login l=new login();
+       l.setVisible(true);
+       l.setSize(1900,700);
+      
+    }
+    
+}
